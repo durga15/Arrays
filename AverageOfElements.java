5bf26f3cb27efabdf88f03ace0c7300366521a58
durga15 <34118507+durga15@users.noreply.github.com>
2025-12-15 01:03:00 +0530

    Create AverageOfElements.java

---
+++ AverageOfElements.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AverageOfElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of Array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " array elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();            
+        }
+        System.out.println("The array elements are: " + Arrays.toString(arr));
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Sum of array elements: " + sum);
+        double avg = sum / n;
+        System.out.println("Average of array elements are: " + avg);
+        sc.close();
+    }
+}
