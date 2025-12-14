50df73ae740eede40d24d487677c559e4094c5b3
durga15 <34118507+durga15@users.noreply.github.com>
2025-12-15 01:04:15 +0530

    Create MinimumElementInArray.java

---
+++ MinimumElementInArray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinimumElementInArray{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of Array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " array elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();            
+        }
+        System.out.println("The array elements are: " + Arrays.toString(arr));
+        int min = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < min) {
+                min =arr[i];
+            } 
+        }
+        System.out.println("Minimum Number is: " + min);
+        sc.close();
+    }
+}
